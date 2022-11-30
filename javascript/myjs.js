c8d6b0c7ecd533c8c7680e84590003751df318ac
Clarisse ISINGIZWE <93644256+ISINGIZWEcla@users.noreply.github.com>
2022-11-30 14:55:42 +0200

    working on responsiveness

---
+++ javascript/myjs.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const navul= document.getElementById('nav-ul');
+
+const menubutton=document.getElementById('menu-btn');
+const menubar=document.getElementById('menub');
+
+hamburger.addEventListener('click', () => {
+  navul.classList.toggle('showNav');
+});
+
+menubutton.addEventListener('click', () => {
+  menubar.classList.toggle('showbtn');
+});
